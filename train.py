c339aed5864cf8301f4f0e4b7b0ae40fb24cd5c8
Sergi Sánchez Ruiz <sergi.sanchezru@gmail.com>
2023-03-11 00:40:01 +0100

    Train Segmentation v1

---
+++ train.py
@@ -0,0 +1,25 @@
+import numpy as np
+from tqdm import tqdm
+from model.segmentation import dice_metric, device
+
+
+def train_func(model, loader_train, optimizer, scaler=None):
+    model.train()
+    train_loss = []
+    bar = tqdm(loader_train)
+    for images, labels in bar:
+        optimizer.zero_grad()
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = dice_metric(outputs, labels)
+
+        train_loss.append(loss.item())
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        bar.set_description(f'train_loss:{np.mean(train_loss):.4f}')
+
+    return np.mean(train_loss)
