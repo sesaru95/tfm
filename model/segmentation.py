02de9bd517a68999a9bba5a37a258e9a4ebdd3f9
Sergi Sánchez Ruiz <sergi.sanchezru@gmail.com>
2023-03-20 14:50:23 +0100

    Run Segmentation v6

--- model/segmentation.py
+++ model/segmentation.py
@@ -1,19 +1,14 @@
 import torch
 import torch.nn as nn
-from monai.networks.nets import UNet
-from monai.networks.layers import Norm
+from monai.networks.nets import densenet121
 from monai.metrics import DiceMetric
 
 # fem servir un model 3D del packet MONAI: UNet
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device('cpu')
-model = UNet(
+model = densenet121(
     spatial_dims=3,
     in_channels=3,
-    out_channels=7,
-    channels=(16, 32, 64, 128, 256),
-    strides=(2, 2, 2, 2),
-    num_res_units=2,
-    norm=Norm.BATCH,
+    out_channels=8,
 ).to(device)
 loss_function = nn.BCEWithLogitsLoss()
 dice_metric = DiceMetric(include_background=False, reduction="mean")
